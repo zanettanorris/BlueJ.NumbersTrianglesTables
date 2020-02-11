6391b2aaeb982b5eb24162587af11fe27f2ba687
Zanetta Norris <znorris@zipcodes-MacBook-Pro-2.local>
2020-02-11 14:26:44 -0500

    Code written for Numbers Triangles Tables lab. All tests pass.

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,32 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
+       String str = "";
+        int i = 1;
+        int j = 1;
+        for  (i= 1; i<=5; i++){
+            for  (j= 1; j<=5; j++){
+            str = str + String.format("%5s", (j*i) +" |");}
+        str = str + "\n";}
+    return str;}
 
     public static String getLargeMultiplicationTable() {
-        return null;
-    }
+           String str = "";
+        int i = 1;
+        int j = 1;
+        for  (i= 1; i<=10; i++){
+            for  (j= 1; j<=10; j++){
+            str = str + String.format("%5s", (j*i) +" |");}
+        str = str + "\n";}
+    return str;}
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
-    }
-}
+        String str = "";
+        int i = 1;
+        int j = 1;
+        for  (i= 1; i<=tableSize; i++){
+            for  (j= 1; j<=tableSize; j++){
+            str = str + String.format("%5s", (j*i) +" |");}
+        str = str + "\n";}
+    return str;}}
+    
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,43 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String str = "";
+        int i = 1;
+        for (i = 1; i <= numberOfStars; i++)
+        str = str + "*";
+        return str;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String str = "";
+        int i = 1;
+        int j;
+        for (i = 1; i <= numberOfRows; i++){
+         for (j= 1; j<=i; j++){
+         str = str + "*";}
+         str = str + "\n";}
+        return str;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String str = "";
+        int i = 1;
+        int j;
+        for (i = 1; i <= 4; i++){
+         for (j= 1; j<=i; j++){
+         str = str + "*";}
+         str = str + "\n";}
+        return str;
     }
 
     public static String getLargeTriangle() {
-        return null;
-    }
+        String str = "";
+        int i = 1;
+        int j;
+        for (i = 1; i <= 9; i++){
+         for (j= 1; j<=i; j++){
+         str = str + "*";}
+         str = str + "\n";}
+        return str;    }
 }
