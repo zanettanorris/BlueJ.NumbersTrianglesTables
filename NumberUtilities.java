aac53b41271ea6db92a9ef9ebcc93d7145205ea9
Zanetta Norris <znorris@zipcodes-MacBook-Pro-2.local>
2020-02-10 19:05:02 -0500

    Finished code for NumberUtilities section. All test cases pass.

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,51 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String str ="";
+        for (int i = 0; i < stop; i++){
+        str = str + i;}
+        return str;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String str ="";
+        for (int i = start; i < stop; i++){
+        str += i;}
+        return str;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String str = "";
+        for (int i = start; i<stop; i+=step){
+        str += i;}
+        return str;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String str ="";
+        if (start % 2 == 0){
+        for (int i = start; i < stop; i+= 2)
+            str += i;
+            return str;}
+        else {for (int i=start + 1; i<stop; i +=2)
+            str += i;
+            return str;}
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String str ="";
+        if (start % 2 == 1);{
+        for (int i = start; i < stop; i+= 2){
+            str += i;}
+            return str;}
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String str ="";
+        for (int i = start; i <= stop; i++){
+        str += (int)Math.pow(i, exponent);}
+        return str;}
     }
-}
